f58060b465e71e0f1a55be103aaa89ce51058491
Linus Ramneborg <uberhaxor101@live.com>
2017-01-05 22:16:24 +0100

    Codebase push
    :fireworks:
    Pushed:
    - Player API
    - Game Textures: decoration, environment, player, icons
    - Central game code
    - Game configuration file

---
+++ main.lua
@@ -0,0 +1,15 @@
+
+
+function love.load()
+  local levelbackgrounds = {}
+  local spriteBatch = {}
+
+end
+
+function love.update()
+
+end
+
+function love.draw()
+
+end
---
+++ static_data/interfaces/player.lua
@@ -0,0 +1,27 @@
+
+local health = 100
+local weapons = {
+  "fists",
+  "sword",
+}
+function hitReg(dt)
+
+end
+
+function movement(dt)
+
+
+end
+
+function physics(dt)
+
+
+end
+
+function update(dt)
+
+end
+
+local function setPos(x,y)
+
+end
